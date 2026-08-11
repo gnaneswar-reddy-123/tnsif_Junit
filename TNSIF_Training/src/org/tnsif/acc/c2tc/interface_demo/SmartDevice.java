e9e3828c0db1cf0822b1bf7d6b457c320150e0ec
gnaneshwar-reddy-123 <iswaryanageswari@gmail.com>
2026-08-11 10:00:47 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/interface_demo/SmartDevice.java
@@ -0,0 +1,11 @@
+package org.tnsif.acc.c2tc.interface_demo;
+
+public interface SmartDevice {
+	public abstract void turnOn();
+	public abstract void turnOff();
+	public abstract void getStatus();
+	
+	
+	
+
+}
